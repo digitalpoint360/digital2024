efc25523e4b51c62e9c5a1f6acafa0c61e4850c9
Nizam Mahmood <nizam@digitalpoint.tech>
2024-10-13 10:04:15 -0400

    Uploading my first code

---
+++ creditCard/src/test/java/creditCard/demo01.java
@@ -0,0 +1,12 @@
+package creditCard;
+
+public class demo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Digital Point Students");
+
+	}
+
+}
